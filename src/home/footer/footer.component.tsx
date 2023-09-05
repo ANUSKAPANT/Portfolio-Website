b2fda8ef077bfd5f5e59bf05f3b3d055f539bc92
anuskapant <anuska.pant@gmail.com>
2023-09-04 21:41:45 -0500

    added header and footer components

---
+++ src/home/footer/footer.component.tsx
@@ -0,0 +1,11 @@
+import './footer.component.scss';
+
+const Footer = () => {
+    return(
+            <footer className="footer-bottom">
+                <p>All rights reserved &copy; <a href="#">Anuska Pant</a></p>
+            </footer>
+    );
+}
+
+export default Footer;
